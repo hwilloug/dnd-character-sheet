05cf7976a677c85a14916afde058bb9a59c4f8d2
Hannah Willoughby <hannah@tcah>
2020-06-01 18:48:41 -0400

    Set up Vue CLI and Express Server

---
+++ dnd-character-sheet/src/components/SpellInfo.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="info-dropdown">
+  
+</div>
+</template>
+
+<script>
+name: "SpellInfo",
+props: [
+  spell
+]
+export default {
+  data: return {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ server/configure.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const api = require('./src/api')
+ 
+module.exports = app => {
+  app.use(bodyParser.json())
+  app.use(morgan('tiny'));
+  app.use(cors());
+  app.use('/api', api);
+}
+                                                                                                                                                                                                           
