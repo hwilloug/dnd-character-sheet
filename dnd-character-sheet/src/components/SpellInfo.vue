482d1434e982da4d314ec7a3e3a9a0da323cc40c
Hannah Willoughby <hannah@tcah>
2020-06-01 20:06:42 -0400

    Start on Spells expandable

--- dnd-character-sheet/src/components/SpellInfo.vue
+++ dnd-character-sheet/src/components/SpellInfo.vue
@@ -1,21 +1,46 @@
 <template>
 <div class="info-dropdown">
-  
+  <button class="collapsible">{{ spell.name }}</button> 
+  <div class="content">
+    <p>Level: {{ spell.level }}</p>
+    <p>Casting time: {{ spell.casting_time }}</p>
+    <p>Range: {{ spell.range }}</p>
+    <p>Components: {{ spell.components }}</p>
+    <p>Duration: {{ spell.duration }}</p>
+    <p>Description: {{ spell.description }}</p>
+  </div>
 </div>
 </template>
 
 <script>
-name: "SpellInfo",
-props: [
-  spell
-]
 export default {
-  data: return {
+  name: "SpellInfo",
+  props: ['spell'],
+  data() { return {
     
-  }
+  }},
 }
 </script>
 
 <style>
+
+.collapsible {
+  background-color: crimson;
+  color: white;
+  width: 50%;
+  cursor: pointer;
+  border: none;
+  font-size: 15px;
+  outline: none;
+  text-align: left;
+}
+
+.collapsible:hover {
+  background-color: #97050f;
+}
+
+.content {
+  width: 50%;
+}
 
 </style>
