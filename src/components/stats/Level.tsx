e7c7fd040ba8061972937d2252b6517ea7185f66
Hannah Willoughby <hannah@hannahwilloughby.dev>
2021-10-10 13:54:35 -0400

    Add level component

---
+++ src/components/stats/Level.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+import { StatsState } from './state/state'
+import { connectRedux, FrameworkComponentProps } from '../../utils/connect'
+import { statsActions } from './state/actions'
+import { useEffect } from 'react'
+
+const StatsTable = styled.table`
+    border: 1px solid black;
+    border-collapse: collapse;
+    & > tbody {    
+        & > tr {
+            & > td {
+                border: 1px solid black;
+                padding: 5px;
+            }
+        }
+    }
+`
+
+
+export type LevelComponentType = FrameworkComponentProps<
+    StatsState,
+    typeof statsActions,
+    {}
+>
+
+export const LevelComponent: React.FunctionComponent<LevelComponentType> =
+    ({state, actions}) => {
+
+        useEffect(() => {
+            actions.getStats('asdf')
+        }, [])
+
+        return (
+            <StatsTable>
+                <tbody>
+                    <tr>
+                        <td>Level</td>
+                        <td>{state.level.level}</td>
+                    </tr>
+                    <tr>
+                        <td>Experience Points</td>
+                        <td>{state.level.experiencePoints}</td>
+                    </tr>
+                    <tr>
+                        <td>Proficiency Bonus</td>
+                        <td>{state.level.proficiencyBonus}</td>
+                    </tr>
+                </tbody>
+            </StatsTable>
+        )
+    }
+
+export function createLevelComponent<
+    TState extends { stats: StatsState }
+>() {
+    return connectRedux(
+        LevelComponent,
+        (state: TState) => state .stats,
+        statsActions, 
+    )
+}
+
+export type Level = ReturnType<typeof LevelComponent>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { AppState } from '../app-store'
 import { createCharacterInfoComponent } from '../components/character-info/CharacterInfo'
-import { createAbilityScoreComponent } from '../components/stats/Stats'
+import { createAbilityScoreComponent } from '../components/stats/AbilityScore'
+import { createLevelComponent } from '../components/stats/Level'
 
 export const HomePageComponent: React.FunctionComponent<{}> =
     () => {
 
         const AbilityScoreComponent = createAbilityScoreComponent<AppState>()
         const CharacterInfoComponent = createCharacterInfoComponent<AppState>()
+        const LevelComponent = createLevelComponent<AppState>()
 
         return (
             <div>
                 <CharacterInfoComponent />
+                <LevelComponent />
                 <AbilityScoreComponent />
             </div>
         )
